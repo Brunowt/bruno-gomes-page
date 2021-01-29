ee6f3cfa31a5a5e11993136737e5b181e84d0418
Brunowt <brunogomes07@outlook.com>
2021-01-29 17:09:17 -0300

    page code start - menu html

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+
+    <title> Bruno Gomes - Desenvolvedor Freelancer </title>
+</head>
+
+<body>
+
+    <div class="container">
+        <!-- Menu -->
+        <div class="nav">
+            <div class="logo">
+                <img src="" alt="Bruno Gomes" class="logo-image">
+            </div>
+            <div class="navbar-items">
+                <ul>
+                    <li class="navbar-item">home</li>
+                    <li class="navbar-item">serviços</li>
+                    <li class="navbar-item">sobre mim</li>
+                    <li class="navbar-item">contato</li>
+
+                </ul>
+            </div>
+        </div>
+        <!-- Home -->
+
+
+        <!-- Services -->
+
+
+        <!-- About Me -->
+
+
+        <!-- Contact -->
+
+    </div>
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
