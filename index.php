1bcf3c63ffc531fcd3494b0803aa351d395d261b
Brunowt <brunogomes07@outlook.com>
2021-01-29 21:47:25 -0300

    start on html and css of first page

--- index.php
+++ index.php
@@ -13,42 +13,62 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
+    <!--Fonts-->
 
     <title> Bruno Gomes - Desenvolvedor Freelancer </title>
 </head>
 
 <body>
 
-    <div class="container">
-        <!-- Menu -->
-        <div class="nav">
-            <div class="logo">
-                <img src="" alt="Bruno Gomes" class="logo-image">
-            </div>
-            <div class="navbar-items">
-                <ul>
-                    <li class="navbar-item">home</li>
-                    <li class="navbar-item">serviços</li>
-                    <li class="navbar-item">sobre mim</li>
-                    <li class="navbar-item">contato</li>
+    <!-- Menu -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background: #000000 !Important;">
+        <div class="container-fluid container">
+            <a class="navbar-brand col-sm-8" href="#">
+                <img src="images/logo.png" alt="Bruno Gomes " class="logo-image">
+            </a>
 
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse navbar-style col-sm-4" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Serviços</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre Mim</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
                 </ul>
             </div>
         </div>
-        <!-- Home -->
+    </nav>
+<!--
+    <div class="main-content">
+        
+        <div class="home-page" style="background: #000000;">
+            <div class="intro-title">
+                WEBSITES <span class="different-font">RESPOSIVOS</span> COM AS TECNOLOGIAS MAIS <span class="different-font">MODERNAS</span>
+            </div>
+            <div class="grey-rectangle">
+
+            </div>
 
 
-        <!-- Services -->
+
+        </div>
 
 
-        <!-- About Me -->
-
-
-        <!-- Contact -->
 
     </div>
-
+-->
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
